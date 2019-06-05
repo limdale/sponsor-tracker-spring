fb9f2e97a63660c226cf05182d895199f63685e2
Dale Lim <ancient.mystic.wonder@gmail.com>
2019-06-05 15:28:26 +1000

    added get single company, 404 error class

--- src/main/java/com/limdale/sponsorpls/controller/CompanyController.java
+++ src/main/java/com/limdale/sponsorpls/controller/CompanyController.java
@@ -1,14 +1,17 @@
 package com.limdale.sponsorpls.controller;
 
+import com.limdale.sponsorpls.exceptions.ResourceNotFoundException;
 import com.limdale.sponsorpls.model.Company;
 import com.limdale.sponsorpls.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/companies")
+@CrossOrigin("http://localhost:3000")
 public class CompanyController {
     @Autowired
     private CompanyRepository companyRepository;
@@ -16,6 +19,16 @@ public class CompanyController {
     @GetMapping("")
     public List<Company> allCompanies() {
         return companyRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Company getCompany(@PathVariable Long id) {
+        Optional<Company> optional = companyRepository.findById(id);
+        if (optional.isPresent()) {
+            return optional.get();
+        } else {
+            throw new ResourceNotFoundException();
+        }
     }
 
     @PostMapping("/new")
