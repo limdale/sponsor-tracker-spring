a150de0de8c112c690bb0670948f238987efbeb2
Dale Lim <ancient.mystic.wonder@gmail.com>
2019-07-15 02:09:35 +1000

    Added better cors filter

--- src/main/java/com/limdale/sponsortracker/auth/WebSecurityConfig.java
+++ src/main/java/com/limdale/sponsortracker/auth/WebSecurityConfig.java
@@ -14,6 +14,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 // JWT Token auth inspired here:
 // https://auth0.com/blog/implementing-jwt-authentication-on-spring-boot/
@@ -31,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .cors().and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/companies/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth/**").permitAll()
@@ -61,5 +68,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedMethods(Arrays.asList("HEAD",
+                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- src/main/java/com/limdale/sponsortracker/controller/CompanyController.java
+++ src/main/java/com/limdale/sponsortracker/controller/CompanyController.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/companies")
-@CrossOrigin("http://localhost:3000")
 public class CompanyController {
     @Autowired
     private CompanyRepository companyRepository;
